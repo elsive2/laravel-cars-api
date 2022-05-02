ba7fa132986b1047e3e6a03ed76c10caf9b2dde6
elsive2 <66211474+elsive2@users.noreply.github.com>
2022-05-02 17:47:05 +0600

    English and Russian localization

---
+++ lang/en/api.php
@@ -0,0 +1,99 @@
+<?php
+
+return [
+
+	/*
+    |--------------------------------------------------------------------------
+    | Api Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | This file is used for messages sent by this api.
+    |
+    */
+
+	'auth' => [
+		'user_not_found' => 'User hasn\'t been found!',
+		'not_user_model' => 'The element isn\'t a user model!',
+		'registered' => 'User has been registered!',
+		'wrong' => 'Wrong email or password!',
+		'logged_out' => 'You have been logged out!',
+	],
+
+	'base' => [
+		'forbidden' => 'Forbidden error!',
+		'not_found' => 'Not found!',
+		'validate' => 'Validate error has occured!',
+		'sent' => 'Data has been sent successfully!',
+
+		'message' => 'message',
+		'data' => 'data',
+		'code' => 'code',
+	],
+
+	'body' => [
+		'not_body_model' => 'The element isn\'t a body model!',
+		'created' => 'Body has been created!',
+		'updated' => 'Body has been updated!',
+		'deleted' => 'Body has been deleted!',
+	],
+
+	'brand' => [
+		'not_brand_model' => 'The element isn\'t a brand model!',
+		'created' => 'Brand has been created!',
+		'updated' => 'Brand has been updated!',
+		'deleted' => 'Brand has been deleted!',
+	],
+
+	'color' => [
+		'not_color_model' => 'The element isn\'t a color model!',
+		'created' => 'Color has been created!',
+		'updated' => 'Color has been updated!',
+		'deleted' => 'Color has been deleted!',
+	],
+
+	'country' => [
+		'not_country_model' => 'The element isn\'t a country model!',
+		'created' => 'Country has been created!',
+		'updated' => 'Country has been updated!',
+		'deleted' => 'Country has been deleted!',
+	],
+
+	'engine' => [
+		'not_engine_model' => 'The element isn\'t an engine model!',
+		'created' => 'Engine has been created!',
+		'updated' => 'Engine has been updated!',
+		'deleted' => 'Engine has been deleted!',
+	],
+
+	'gear_box' => [
+		'not_gear_box_model' => 'The element isn\'t a gear box model!',
+		'created' => 'Gear box has been created!',
+		'updated' => 'Gear box has been updated!',
+		'deleted' => 'Gear box has been deleted!',
+	],
+
+	'image' => [
+		'not_found' => 'Image hasn\'t been found!',
+		'not_image_model' => 'The element isn\'t an image model!',
+		'used' => "Image number $id is already in use",
+		'saved' => 'Images have been saved!',
+		'loaded' => 'Image hasn\'t been loaded!',
+		'deleted' => 'Image has been deleted!',
+		'deleted_plural' => 'Images have been deleted!',
+	],
+
+	'options' => [
+		'not_found' => 'Options hasn\'t been found!',
+		'not_option_model' => 'The element isn\'t a option model!',
+	],
+
+	'car' => [
+		'not_found' => 'Car hasn\'t been found!',
+		'not_car_model' => 'The element isn\'t a car model',
+		'success' => 'Success! Your car will be published after moderation!',
+		'updated' => 'Car has been updated!',
+		'deleted' => 'Car has been deleted!',
+	],
+
+	'smth_wrong' => 'Something went wrong!',
+];
